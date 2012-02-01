080c49fbbebd5217a5eababf72d7da96b80bfb8a
Hoshi Takanori <hoshi@hoshi-air13.local>
2012-02-01 20:57:57 +0900

    Initial Commit

---
+++ CloudPanel/CloudDocument.h
@@ -0,0 +1,13 @@
+//
+//  CloudDocument.h
+//  CloudPanel
+//
+//  Created by Hoshi Takanori on 12/02/01.
+//  Copyright (c) 2012年 -. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CloudDocument : NSDocument
+
+@end
---
+++ CloudPanel/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CloudPanel
+//
+//  Created by Hoshi Takanori on 12/02/01.
+//  Copyright (c) 2012年 -. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
